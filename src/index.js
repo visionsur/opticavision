af9d2812380465aa135e6cc9a3f39da11b7fe5ca
visionsur <visionsur2023@gmail.com>
2023-06-30 07:06:09 -0300

    1.0.2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import NavBar from './navbar/navbar';
 ReactDOM.render(
     <App />,
   document.getElementById('root')
